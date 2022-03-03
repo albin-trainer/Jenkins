f7b9e693421f606e7762bd16523129f743e72f28
albin-trainer <69448620+albin-trainer@users.noreply.github.com>
2022-03-03 17:41:22 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -2,6 +2,6 @@ public class Jenkins{
 
 public static void main(String [] args){
 System.out.println("Hello jenkins, i m coming from GitHub");
-  System.out.println("Hello");
+  System.out.println("Hello All");
 }
 }
